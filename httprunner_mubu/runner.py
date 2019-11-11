4ab3dd74486c176ad84c63409db2416c3f5a8e8d
smellwithsun <1354350240@qq.com>
2019-11-12 00:06:03 +0800

    feated:add jsonpath assert

--- httprunner_mubu/runner.py
+++ httprunner_mubu/runner.py
@@ -1,6 +1,10 @@
 import requests
+import jsonpath
 
 from httprunner_mubu.load import load_yaml
+def extact_json_field(resp,json_field):
+    value =jsonpath.jsonpath(resp.json(),json_field)
+    return value[0]
 def run_yaml(yml_file):
     load_json = load_yaml(yml_file)
     request = load_json["request"]
@@ -12,7 +16,10 @@ def run_yaml(yml_file):
     validator_mapping =load_json["validate"]
 
     for key in validator_mapping:
-        actual_key = getattr(resp,key) #resp.key
+        if "$" in key:
+            actual_key = extact_json_field(resp,key)
+        else:
+            actual_key = getattr(resp,key) #resp.key
         expected_value = validator_mapping[key]
         assert actual_key == expected_value
 
